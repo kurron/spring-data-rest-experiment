3fcb4407231055ce1d3f174251047d6cfe53e283
Ronald D. Kurr <kurr@kurron.org>
2013-04-04 13:27:11 -0400

    Added Spring Data Rest configuration to the mix.

---
+++ src/main/java/org/kurron/mvc/RestConfiguration.java
@@ -0,0 +1,9 @@
+package org.kurron.mvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+@Configuration
+public class RestConfiguration extends RepositoryRestMvcConfiguration {
+
+}
--- src/main/java/org/kurron/mvc/SpringConfiguration.java
+++ src/main/java/org/kurron/mvc/SpringConfiguration.java
@@ -3,6 +3,8 @@ package org.kurron.mvc;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -13,6 +15,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan( {"org.kurron.mvc"} )
+//@Import( RepositoryRestMvcConfiguration.class )
 public class SpringConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
