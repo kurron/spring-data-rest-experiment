c386084fc24020c7e29842ae7c5f3b9716aac972
Ron Kurr <kurr@kurron.org>
2017-07-07 15:09:28 -0400

    Tring to make the repository read-only but I can't seem to get the save methods out.

--- src/main/groovy/org/kurron/sdr/ModelRepository.groovy
+++ src/main/groovy/org/kurron/sdr/ModelRepository.groovy
@@ -1,8 +1,26 @@
 package org.kurron.sdr
 
 import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.data.rest.core.annotation.RestResource
 
 /**
  * Handles interaction with MongoDB.
  */
-interface ModelRepository extends PagingAndSortingRepository<Model, UUID> {}
+interface ModelRepository extends PagingAndSortingRepository<Model, UUID> {
+
+    @Override
+    @RestResource(exported = false)
+    void delete(UUID uuid)
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Model entity)
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Iterable<? extends Model> entities)
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll()
+}
