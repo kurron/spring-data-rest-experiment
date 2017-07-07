674352850e278e761d337b33fcaa4094b31948d4
Ron Kurr <kurr@kurron.org>
2017-07-07 15:13:02 -0400

    Added CORS support.

--- src/main/groovy/org/kurron/sdr/ModelRepository.groovy
+++ src/main/groovy/org/kurron/sdr/ModelRepository.groovy
@@ -2,10 +2,13 @@ package org.kurron.sdr
 
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.rest.core.annotation.RestResource
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RequestMethod
 
 /**
  * Handles interaction with MongoDB.
  */
+@CrossOrigin( methods = [RequestMethod.GET, RequestMethod.HEAD, RequestMethod.OPTIONS])
 interface ModelRepository extends PagingAndSortingRepository<Model, UUID> {
 
     @Override
